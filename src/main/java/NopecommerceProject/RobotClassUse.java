c5434fe8a14e94a1dc2d0b094bf1a46a0cdb7f5b
Dilip Jadav <ddjadav@gmail.com>
2017-01-11 02:13:32 +0000

    robotclass

---
+++ src/main/java/NopecommerceProject/RobotClassUse.java
@@ -0,0 +1,38 @@
+package NopecommerceProject;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+
+import java.awt.*;
+//import java.awt.Dimension;
+
+import static NopecommerceProject.DriverManager.driver;
+
+/**
+ * Created by sreenivas on 08/12/2016.
+ */
+public class RobotClassUse {
+    public static void main(String[] args)throws AWTException {
+
+
+//        WebElement webele = driver.findElement(By.className("ico-login"));
+//        Point p = webele.getLocation();
+//        System.out.println("login location"+p);
+//        int x = p.getX();
+//        System.out.println("x= "+x);
+//        int y = p.getY();
+//        System.out.println("y= "+y);
+//        Dimension d = webele.getSize();
+//        System.out.println("dimention "+d);
+//        int h = d.getHeight();
+//        System.out.println("h= "+h);
+//        int w = d.getWidth();
+//        System.out.println("w= "+w);
+//        Robot r = new Robot();
+//        r.mouseMove(x + (w/2), y+(h/2) +80);
+
+        System.out.println(1071+(33/2)+","+0+(45/2)+80);
+        System.out.println((45/2)+80);
+    }
+}
